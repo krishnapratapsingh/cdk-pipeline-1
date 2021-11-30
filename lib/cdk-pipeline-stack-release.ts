90a1ec748bb3353862260a6f63ba491cda3d6c0a
krishna <krishan.singh@genpact.com>
2021-11-30 06:30:32 +0000

    added

--- lib/cdk-pipeline-stack-release.ts
+++ lib/cdk-pipeline-stack-release.ts
@@ -1,10 +1,12 @@
 import { Stage, Construct, StageProps,} from '@aws-cdk/core';
-
+import { FirstCftPipelineStack } from './stacks/myFirstChildPipeline';
 
 
 
 export class CdkchildpipelineRelease extends Stage {
   constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props);
+    const FirstChildRef = new FirstCftPipelineStack(this, 'my-First-Child-Pipeline');
+    FirstChildRef.templateOptions.description = 'First-pipeline';
   }
 }

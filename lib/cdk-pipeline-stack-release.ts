cd867bbb5c1c05d383c623815ab707637b5565ae
krishna <krishan.singh@genpact.com>
2021-12-01 10:19:27 +0000

    new satck

--- lib/cdk-pipeline-stack-release.ts
+++ lib/cdk-pipeline-stack-release.ts
@@ -1,6 +1,6 @@
 import { Stage, Construct, StageProps,} from '@aws-cdk/core';
 import { FirstCftPipelineStack } from './stacks/myFirstChildPipeline';
-
+import { cfnlintProjStack } from './stacks/cfn-lint-stack';
 
 
 export class CdkchildpipelineRelease extends Stage {
@@ -8,5 +8,7 @@ export class CdkchildpipelineRelease extends Stage {
     super(scope, id, props);
     const FirstChildRef = new FirstCftPipelineStack(this, 'my-First-Child-Pipeline');
     FirstChildRef.templateOptions.description = 'First-pipeline';
+        const cfnlintObj = new cfnlintProjStack( this, "SyntexValidationProject");
+    cfnlintObj.templateOptions.description = 'SyntexValidationProject';
   }
 }
